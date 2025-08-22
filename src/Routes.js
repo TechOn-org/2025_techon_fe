c04372fcd93a183c21a2557da53403440f43f5dd
dusskdus <skdusb0007@naver.com>
2025-08-22 16:37:10 +0900

    feat: 진단페이지 애니메이션 추가 및 페이지연결

--- src/Routes.js
+++ src/Routes.js
@@ -6,17 +6,21 @@ import MainPage from "./pages/main/main.jsx";
 import SelectPage from "./pages/fix/select.jsx";
 import WritePage from "./pages/fix/write.jsx";
 import LocationPage from "./pages/fix/location.jsx";
+import SearchPage from "./pages/fix/search.jsx";
 import EstimatePage from "./pages/estimate/estimate.jsx";
 import MatchPage from "./pages/matching/matching.jsx";
+import Login from "./pages/login/main.jsx";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/main" element={<MainPage />} />
       <Route path="/select" element={<SelectPage />} />
       <Route path="/write" element={<WritePage />} />
       <Route path="/location" element={<LocationPage />} />
+      <Route path="/search" element={<SearchPage />} />
       <Route path="/estimate" element={<EstimatePage />} />
       <Route path="/match" element={<MatchPage />} />
     </Routes>
