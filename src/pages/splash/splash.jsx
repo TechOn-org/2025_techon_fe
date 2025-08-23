0af21eb47e40e56d4ae6b244fda204b25abec9b2
Tanyi <gudwn0125@gmail.com>
2025-08-23 14:46:18 +0900

    container css 통일

--- src/pages/splash/splash.jsx
+++ src/pages/splash/splash.jsx
@@ -12,21 +12,18 @@ export default function Splash() {
     const t2 = setTimeout(() => setPhase("logo"), TEXT_HOLD + FADE + GAP);
     const t3 = setTimeout(() => navigate("/login"), TEXT_HOLD + FADE + GAP + 2000);
     return () => { clearTimeout(t1); clearTimeout(t2); clearTimeout(t3);};
-
   }, []);
 
   return (
-    <div className="splash-wrap">
-      <div className="phone-bg">
-        <section className={`layer ${phase === "text" ? "textshow" : phase === "fading" ? "fadeout" : "hide"}`}>
-          <h1 className="slogan">AI로 고치고, 지구를 살리다.</h1>
-        </section>
-        <section className={`layer ${phase === "logo" ? "show" : "hide"}`}>
-          <div className="logo-container">
-            <img className="logo" src="/images/logo_icon.svg" alt="Tech On 로고" />
-          </div>
-        </section>
-      </div>
+    <div className="container">
+      <section className={`layer ${phase === "text" ? "textshow" : phase === "fading" ? "fadeout" : "hide"}`}>
+        <h1 className="slogan">AI로 고치고, 지구를 살리다.</h1>
+      </section>
+      <section className={`layer ${phase === "logo" ? "show" : "hide"}`}>
+        <div className="logo-container">
+          <img className="logo" src="/images/logo_icon.svg" alt="Tech On 로고" />
+        </div>
+      </section>
     </div>
   );
 }
